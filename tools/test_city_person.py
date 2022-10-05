96d3acc8366dcdd30ead675bb4dccb0c4b1c80fb
HarukiHonda <60038634+HaruHonda@users.noreply.github.com>
2022-10-05 11:30:54 +0200

    Update test_city_person.py

--- tools/test_city_person.py
+++ tools/test_city_person.py
@@ -217,7 +217,7 @@ def main():
         with open(args.out, 'w') as f:
             json.dump(res, f)
 
-        MRs = validate('datasets/CityPersons/val_gt.json', args.out)
+        MRs = validate('/content/drive/MyDrive/Pedestrian/datasets/CityPersons/val_gt.json', args.out)
         print('Checkpoint %d: [Reasonable: %.2f%%], [Reasonable_Small: %.2f%%], [Heavy: %.2f%%], [All: %.2f%%]'
               % (i, MRs[0] * 100, MRs[1] * 100, MRs[2] * 100, MRs[3] * 100))
 
